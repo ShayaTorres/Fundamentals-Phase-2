9b736b46b5835e4e8209f52a579f89e405ddacbf
Shaya Torres <storres133@cnm.edu>
2024-07-05 13:31:48 -0600

    added fundamental lessons

---
+++ variables-and-data.js
@@ -0,0 +1,42 @@
+/*
+*Variables, Data Types, and Typing
+ */
+let wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+/*...*/
+//
+
+let obj = {
+    key1: "value 1",
+    key2: 3,
+    boolean: true,
+    obj2: {
+        obj2Key1 : "value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2Key1);
+
+//
+
+let variable: string = "string";
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+let string = "String 1 " + "String 2"''
+
+console.log(string);
+
+
+
+
+
